f2df1c45ca82f64598255e0561fe9ed284d29201
Cat Norris <cannenorris@yahoo.com>
2017-11-08 23:55:15 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
 def unsafe?(speed)
-
+if speed < 40 then
+	true
+elsif speed >60 then
+	true
+elsif speed >=40 && speed <=60 then
+	false
+end
 end
 
 
 
 def not_safe?(speed)
-	
+speed >60 ? true:false
+speed <40 ? true:false
+speed >=40 && speed <=60 ? false :true
 end
-	
-
-
