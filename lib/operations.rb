aa25138bd9d5df0f8c59249d8d940fb7a47b6806
Cat Norris <cannenorris@yahoo.com>
2017-11-08 23:46:25 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,9 @@
 def unsafe?(speed)
-if speed < 40 
+if speed < 40 then
 	true
-elsif speed >60 
+elsif speed >60
 	true
-elsif speed =>40 && speed <=60 
+elsif speed =>40 && speed <=60
 	false
 end
 end
